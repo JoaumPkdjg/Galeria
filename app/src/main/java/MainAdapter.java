b54f3486e694b7bc072976dfa74d66cabf140d1f
20201TIIMI0098 <jaooeiid@gmail.com>
2022-09-23 10:05:51 -0300

    Desenvolvimento da atividade da Galeria

--- app/src/main/java/MainAdapter.java
+++ app/src/main/java/MainAdapter.java
@@ -4,11 +4,11 @@ import java.util.List;
 
 import eid.joao.galeria.MainActivity;
 
-public class MainAdapter extends RecyclerView.Adapter {
+/*public class MainAdapter extends RecyclerView.Adapter {
     MainActivity mainActivity;
     List<String> photos;
 
     public MainAdapter(MainActivity mainActivity, List<String> photos) {
 
     }
-}
+}*/
